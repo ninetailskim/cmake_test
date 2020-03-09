5b346c2f808afead20a6018464dc61430ab1f3a5
thirdbomb <iregisteru@163.com>
2020-03-09 16:08:29 +0800

    '0309'

---
+++ 0309001/include/hello_cv_1.h
@@ -0,0 +1,20 @@
+#ifndef HELLO_CV_1_H_
+#define HELLO_CV_1_H_
+
+#include <string>
+#include <iostream>
+
+namespace Hello_cv_1
+{
+    class HelloCv_1
+    {
+        public:
+            HelloCv_1();
+            ~HelloCv_1();
+            std::string getMsgsContent(std::string say_something);
+        private:
+            std::string say_something;
+    };
+}
+
+#endif
